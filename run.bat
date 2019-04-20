347b9e188a93b15c2324dc16bc1b340e1d02ed0f
bcyy <1249799972@qq.com>
2019-04-20 23:19:54 +0800

    Signed-off-by: bcyy <1249799972@qq.com>

---
+++ run.bat
@@ -0,0 +1,6 @@
+echo¡°££yoyosoft.BookList¡±>> README.md 
+git init 
+git add README.md 
+git commit -m¡°first commit¡± 
+git remote add origin git@github.com:52bcyy/yoyosoft.BookList.git
+git push -u origin master
